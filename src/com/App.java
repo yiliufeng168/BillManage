4e4e7d82abb3fc44992abfcf8510a8a3fbaed25f
yiliufeng168 <2440446556@qq.com>
2021-01-07 16:30:49 +0800

    代码优化

--- src/com/App.java
+++ src/com/App.java
@@ -9,13 +9,12 @@ import com.ui.MenuUi;
 
 public class App {
 	public static void main(String[] args) {
-        new InitSystem();
 
         LoginUi loginUi =new LoginUi();
-//        loginUi.CreateJFrame("个人记账系统");
-        MenuUi m=new MenuUi("主菜单", UserService.login(new User(null, "小公主", "123456", null, null, null)));
+        loginUi.CreateJFrame("个人记账系统");
+//        MenuUi m=new MenuUi("主菜单", UserService.login(new User(null, "小公主", "123456", null, null, null)));
 //        SetEnterUi setEnterUi=new SetEnterUi(null,UserBiz.Login(new User(null, "ylf", "123", null, null, null)),0);
 //        BillQueryUI billQueryUI=new BillQueryUI(null, UserBiz.Login(new User(null, "小公主", "123456", null, null, null)));
-	 
+
 	}
 }
